67874fb646b7d5aec487c9487b8444923ec0c171
Juri Young <olivinova7@gmail.com>
2023-07-02 13:33:17 -0500

    adding basic physics library stubs to build out later

--- herculoid/src/main.rs
+++ herculoid/src/main.rs
@@ -1,7 +1,12 @@
 use bevy::{DefaultPlugins, prelude::App};
-
+use acceleration::{};
+use physics_manager::{};
+use collision::{};
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_plugin(acceleration)
+    .add_plugin(physics_manager)
+    .add_plugin(collision)
     .run();
 }
